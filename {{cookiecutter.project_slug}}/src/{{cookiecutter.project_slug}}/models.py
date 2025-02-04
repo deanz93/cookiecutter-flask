5c0e42b9e62f9e3b261ac368000fd70f0dc7eacc
Najmuddin Yusoff <najmuddin@plisca.com.my>
2025-02-04 12:38:53 +0800

    add models for track module and log

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/models.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/models.py
@@ -1,5 +1,17 @@
+from datetime import datetime
+
+from database.core import Mixin
 from .extensions import db
 
-class Example(db.Model):
+class Module(Mixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    version = db.Column(db.String(20), default='1.0.0')  # Versioning
+    enabled = db.Column(db.Boolean, default=False)
+    installed_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Log(Mixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    module_name = db.Column(db.String(50))
