ba4c75e90a8ded6433b5f279b2b4bc6b7ec2e165
Najmuddin Yusoff <najmuddin@plisca.com.my>
2025-02-04 10:59:22 +0800

    fix swagger

--- {{cookiecutter.project_slug}}/app/views.py
+++ {{cookiecutter.project_slug}}/app/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, jsonify, request
-{% if cookiecutter.use_swagger == 'y' %}from flasgger import swag_from{% endif %}
+{% if cookiecutter.use_swagger == 'y' %}from flasgger import swag_from
+from .extensions import swagger{% endif %}
+
 
 bp = Blueprint('views', __name__)
+
 
 @bp.route('/')
 {% if cookiecutter.use_swagger == 'y' %}@swag_from({
@@ -12,17 +15,47 @@ bp = Blueprint('views', __name__)
 def index():
     return jsonify({"message": "Welcome to {{ cookiecutter.project_name }}"})
 
+
+{% if cookiecutter.use_swagger == 'y' %}@swagger.definition('Sum')
+class Sum(object):
+    """
+    Sum Object
+    ---
+    properties:
+        a:
+            type: integer
+        b:
+            type: integer
+    """
+    def __init__(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+
+    def dump(self):
+        return dict(vars(self).items())
+{% endif %}
+
 @bp.route('/api/v1/sum', methods=['POST'])
-{% if cookiecutter.use_swagger == 'y' %}@swag_from({
-    'parameters': [
-        {'name': 'a', 'in': 'formData', 'type': 'integer', 'required': True},
-        {'name': 'b', 'in': 'formData', 'type': 'integer', 'required': True}
-    ],
-    'responses': {
-        200: {'description': 'Sum result', 'examples': {'application/json': {'result': 5}}}
-    }
-}){% endif %}
 def sum_numbers():
+    {% if cookiecutter.use_swagger == 'y' %} """
+    An endpoint for testing requestBody documentation.
+    ---
+    description: Post a request body
+    requestBody:
+        content:
+            application/json:
+                schema:
+                    $ref: '#/components/schemas/Sum'
+        required: true
+    responses:
+        200:
+            description: The posted request body
+            content:
+                application/json:
+                    schema:
+                        $ref: '#/components/schemas/Sum'
+    """
+    {% endif %}
     a = int(request.form.get('a'))
     b = int(request.form.get('b'))
     return jsonify({'result': a + b})
