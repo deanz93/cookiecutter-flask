b2ac44acda8c03ab25f3023f6ab59b63be811960
Najmuddin Yusoff <najmuddin@plisca.com.my>
2025-02-05 11:20:41 +0800

    wrap app

--- {{cookiecutter.project_slug}}/src/run.py
+++ {{cookiecutter.project_slug}}/src/run.py
@@ -1,6 +1,8 @@
 from {{cookiecutter.project_slug}} import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run(debug=app.config['DEBUG'], host="0.0.0.0", port=5000)
