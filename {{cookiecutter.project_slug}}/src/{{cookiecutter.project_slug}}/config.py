ffa71b6c19fbb8e6a542cec60c20635a79db5bda
Najmuddin Yusoff <najmuddin@plisca.com.my>
2025-02-05 14:53:15 +0800

    fix email error

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/config.py
@@ -20,19 +20,21 @@ class Config:
     STATIC_PATH = os.path.abspath(os.getenv("STATIC_PATH", 'staticfiles'))
     TESTING = os.getenv('FLASK_TESTING', 'False')
     TEMPLATES_AUTO_RELOAD = True
-    {% if cookiecutter.use_cloud_storage == 'y' %}
+{% if cookiecutter.use_cloud_storage == 'y' %}
     # Cloud Storage
     S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
     S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
     S3_ENDPOINT = os.getenv("S3_ENDPOINT")
     S3_REGION = os.getenv("S3_REGION")
     S3_BUCKET = os.getenv("S3_BUCKET")
-    {% endif %}
+{% endif %}
     # Database
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///default.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    {% if cookiecutter.use_email_service == 'y' %}
+{% if cookiecutter.use_email_service == 'y' %}
     MAIL_SERVER = os.getenv("MAIL_SERVER", 'localhost')
+    MAIL_DEBUG = ast.literal_eval(
+        os.getenv('MAIL_DEBUG', 'True'))
     MAIL_PORT = int(os.getenv("MAIL_PORT", '25'))
     MAIL_USE_TLS = ast.literal_eval(
         os.getenv('MAIL_USE_TLS', 'True'))
@@ -40,19 +42,18 @@ class Config:
         os.getenv('MAIL_USE_SSL', 'False'))
     MAIL_USERNAME = os.getenv("MAIL_USERNAME", '')
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", '')
-    MAIL_DEFAULT_SENDER = os.getenv("DEFAULT_FROM_EMAIL"){% endif %}
-
+    MAIL_DEFAULT_SENDER = os.getenv("DEFAULT_FROM_EMAIL")
+{% endif %}
     # CORS
     CORS_HEADERS = 'Content-Type'
     ALLOWED_ORIGINS = os.getenv('ALLOWED_ORIGINS', '*')  # e.g., 'https://example.com'
-
-    {% if cookiecutter.use_celery == 'y' %}# Redis Settings
+{% if cookiecutter.use_celery == 'y' %}# Redis Settings
     REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
     # Celery Settings
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'){% endif %}
-
-    {% if cookiecutter.use_swagger == 'y' %}SWAGGER = {
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+{% endif %}
+{% if cookiecutter.use_swagger == 'y' %}SWAGGER = {
         'title': '{{ cookiecutter.project_name }} API',
         'uiversion': 3,
         'openapi': '3.0.2'
