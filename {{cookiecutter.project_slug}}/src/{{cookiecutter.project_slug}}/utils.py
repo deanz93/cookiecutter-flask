71c9844f6f0fefda73afceb77006d3e8b566a703
Najmuddin Yusoff <najmuddin@plisca.com.my>
2025-02-12 23:26:00 +0800

    fix utils

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/utils.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/utils.py
@@ -4,7 +4,8 @@ from .extensions import db
 
 
 # Logging function
-def log_action(action, module_name=None):
-    log_entry = Log(action=action, module_name=module_name)
+def log_action(action, module_id=None):
+    log_entry = Log(action=action, module_id=module_id)
     db.session.add(log_entry)
     db.session.commit()
+
